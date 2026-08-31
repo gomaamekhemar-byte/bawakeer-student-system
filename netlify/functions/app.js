7ae4f8ac837585ffbc008df9db0cb2cdebfe5c97
gomaamekhemar-byte <gomaamekhemar@gmail.com>
2026-08-31 12:52:17 +0300

    Update: netlify/functions/app.js

--- netlify/functions/app.js
+++ netlify/functions/app.js
@@ -1,4 +1,13 @@
-﻿const serverless = require("serverless-http");
+const serverless = require("serverless-http");
 const app = require("../../src/app");
 
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  binary: [
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    "application/vnd.ms-excel",
+    "application/octet-stream",
+    "application/pdf",
+    "image/*",
+    "*/*"
+  ]
+});
